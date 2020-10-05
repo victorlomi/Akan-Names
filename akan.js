fd03d4ae74adaede652eebb52e3226d0089b69e4
victorlomi <victorlominyo@gmail.com>
2020-10-05 12:42:42 +0300

    apply adjustMonth to getDay

--- akan.js
+++ akan.js
@@ -13,6 +13,7 @@ let adjustMonth = function(month) {
 
 let getDay = function(day, month, year, century) {
     // This function returns the day of the week based on date
+    month = adjustMonth(month);
     return (day + (Math.floor(2.6 * month - 0.2)) - (2 * century) + year + (Math.floor(year / 4)) + (Math.floor(century / 4))) % 7;
 };
 
